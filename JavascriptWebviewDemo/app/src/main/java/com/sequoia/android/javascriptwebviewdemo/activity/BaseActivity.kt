43801b34dec37a47c2cdce4ad239646019baa80e
Ankita Bajaj <ankita.bajaj@sequoia.com>
2020-07-08 10:29:46 +0530

    Javascript Webview Demo Project

---
+++ JavascriptWebviewDemo/app/src/main/java/com/sequoia/android/javascriptwebviewdemo/activity/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.sequoia.android.activity
+
+import androidx.appcompat.app.AppCompatActivity
+
+/*
+ * Copyright 2019 Sequoia Consulting Group
+ * License details with company address
+ * 1850 Gateway Dr, Ste 700
+ * San Mateo, CA 94404
+ *(650) 369.0200
+ */
+
+/**
+ * Created by Ankita Bajaj on 05/07/20.
+ */
+open class BaseActivity : AppCompatActivity() {
+
+    open fun callApi() {
+       // no implementation
+    }
+}
---
+++ JavascriptWebviewDemo/app/src/main/java/com/sequoia/android/javascriptwebviewdemo/model/UserDetails.kt
@@ -0,0 +1,14 @@
+package com.sequoia.android.model
+
+/*
+ * Copyright 2019 Sequoia Consulting Group
+ * License details with company address
+ * 1850 Gateway Dr, Ste 700
+ * San Mateo, CA 94404
+ *(650) 369.0200
+ */
+
+/**
+ * Created by Ankita Bajaj on 06/07/20.
+ */
+data class UserDetails(val name: String, val email: String, val phoneNumber: String)
